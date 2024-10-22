f8233e580739984e13f54bda243a4a3df20acf79
MoonKyoungHun <lsf8355@gmail.com>
2024-10-22 13:06:56 +0900

    [Feat] 총알 구현
    작동 시 나가는 총알 구현

---
+++ Assets/Scenes/Script/Shooter.cs
@@ -0,0 +1,30 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class Shooter : MonoBehaviour
+{
+    [SerializeField] GameObject bulletPrefab = null;
+
+    [SerializeField] Transform shootPoint = null;
+
+    [SerializeField] float bulletSpeed = 1.0f;
+
+    void ApplyForce(Rigidbody rigid)
+    {
+        Vector3 force = shootPoint.forward * bulletSpeed;
+        rigid.AddForce(force);
+    }
+
+    public void OnFire()
+    {
+
+        GameObject bullet = Instantiate(bulletPrefab, shootPoint.position, shootPoint.rotation, null);
+
+        if (bullet.TryGetComponent(out Rigidbody rigid))
+            ApplyForce(rigid);
+
+        Destroy(bullet, 10f);
+
+    }
+
+}
